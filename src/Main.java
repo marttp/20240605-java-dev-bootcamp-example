2eb9c795fca682f84b3011a8a70f6b2a23e04466
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-06 15:03:42 +0900

    Change main method to instance main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to example!");
-    }
+void main() {
+    System.out.println("Welcome to example!");
 }
