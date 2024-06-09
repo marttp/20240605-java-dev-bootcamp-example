6f156f61e86ac383a5a5434aac633e53303959ac
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 00:36:51 +0900

    Inheritance - Animal

---
+++ src/oop/inheritance/Animal.java
@@ -0,0 +1,18 @@
+package oop.inheritance;
+
+public class Animal {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println(name);
+    }
+}
