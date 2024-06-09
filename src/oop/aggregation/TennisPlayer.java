c7dffcba5bff47abc37f3f4e4a0c5ddeca203d80
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 00:49:56 +0900

    Aggregation - TennisPlayer

---
+++ src/oop/aggregation/TennisPlayer.java
@@ -0,0 +1,28 @@
+package oop.aggregation;
+
+public class TennisPlayer {
+
+    private String name;
+    private Racket racket;
+
+    public TennisPlayer(String name, Racket racket) {
+        this.name = name;
+        this.racket = racket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Racket getRacket() {
+        return racket;
+    }
+
+    public void setRacket(Racket racket) {
+        this.racket = racket;
+    }
+}
