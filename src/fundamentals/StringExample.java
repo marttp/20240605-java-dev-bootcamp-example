e556b26d64c3263db2412548ddba4fc6989afeca
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-09 13:10:59 +0900

    Continue on String & StringBuilder topic

---
+++ src/fundamentals/StringExample.java
@@ -0,0 +1,63 @@
+package fundamentals;
+
+import java.util.Arrays;
+
+public class StringExample {
+
+    // String - data type for text on Java (almost every programming languages)
+    // String - immutable, reference type -> Store on heap
+    // String variableName = "ตัวอย่างข้อมูล";
+    void main() {
+        stringExample();
+        System.out.println("======");
+        stringBuilderExample();
+        System.out.println("======");
+        // "name:exp:nationality"
+        String name = "Mart";
+        int exp = 5;
+        String nationality = "Thai";
+        // New Java
+        System.out.println(STR."\{name}:\{exp}:\{nationality}");
+    }
+
+    private static void stringExample() {
+        String name = "TP Coder";
+        // name = "TP Coder, exp 5 years"; // Reassign
+        // name = name + ", exp 5 years"; // Concat 1
+        name = name.concat(", exp 5 years"); // Concat 2
+        System.out.println(name);
+        // String - array of character
+        char[] nameCharArray = name.toCharArray();
+        System.out.println(nameCharArray);
+        System.out.println(Arrays.toString(nameCharArray));
+        // String - has many utility methods
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        // Compare string - use equals
+        var ex1 = "TP";
+        var ex2 = "tp";
+        System.out.println(ex1.equals(ex2)); // false
+        System.out.println(ex1.equalsIgnoreCase(ex2)); // true
+    }
+
+    private static void stringBuilderExample() {
+        // "name:exp:nationality"
+        String name = "Mart";
+        int exp = 5;
+        String nationality = "Thai";
+
+        // String key = name + exp + nationality; // Concat 2 times, generate in background 2 times
+        String key = name + ":" + exp + ":" + nationality; // Concat 4 times
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(":");
+        sb.append(exp);
+        sb.append(":");
+        sb.append(nationality);
+        String keyBySb = sb.toString(); // Generate 1 time
+
+        System.out.println(key);
+        System.out.println(keyBySb);
+    }
+}
