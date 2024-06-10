547db7b22dca9eea3024a65f8b0242a3a22b80cd
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 23:33:40 +0900

    Definition of Single Responsibility Principle

---
+++ src/oop/solid/s/Main.java
@@ -0,0 +1,11 @@
+package oop.solid.s;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        * Single Responsibility Principle (SRP)
+        * One class should have one, and only one, responsibility.
+        * */
+    }
+}
