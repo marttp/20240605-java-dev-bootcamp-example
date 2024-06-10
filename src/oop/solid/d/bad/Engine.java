9baac6ba42eb47736abe285db9e34e140f63674a
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 23:41:05 +0900

    D - bad - Engine

---
+++ src/oop/solid/d/bad/Engine.java
@@ -0,0 +1,32 @@
+package oop.solid.d.bad;
+
+public class Engine {
+
+    private String brand;
+    private int horsepower;
+
+    public Engine(String brand, int horsepower) {
+        this.brand = brand;
+        this.horsepower = horsepower;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public void start() {
+        System.out.println("Start Engine!!!!!");
+    }
+}
