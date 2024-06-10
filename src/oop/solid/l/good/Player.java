649b6a4e5c92ae1eb910c8e8d98172bdaca5139a
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 23:38:57 +0900

    L - good - Player

---
+++ src/oop/solid/l/good/Player.java
@@ -0,0 +1,11 @@
+package oop.solid.l.good;
+
+public abstract class Player implements ViewBattleRoyal {
+
+    private final String name;
+
+    protected Player(String name) {
+        this.name = name;
+    }
+
+}
