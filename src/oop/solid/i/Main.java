a8da0532b5b1727f134a70629dceeca66370de19
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-10 23:34:11 +0900

    Definition of Interface Segregation Principle

---
+++ src/oop/solid/i/Main.java
@@ -0,0 +1,12 @@
+package oop.solid.i;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * Interface Segregation Principle (ISP)
+         * Clients should not be forced to implement unnecessary methods that they will not use.
+         * Many client-specific interfaces are better than one general-purpose interface.
+         * */
+    }
+}
