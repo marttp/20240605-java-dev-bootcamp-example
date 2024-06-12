13624184594edcacaa44111629ca5fc1be5cf868
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-13 00:28:19 +0900

    Collections Framework - TreeMapExample

---
+++ src/intermediate/collections/map/TreeMapExample.java
@@ -0,0 +1,26 @@
+package intermediate.collections.map;
+
+import intermediate.collections.model.Account;
+import intermediate.collections.model.User;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+        // Key wil sorted
+        TreeMap<Long, Account> userIdMapAccount = new TreeMap<>();
+        List<User> users = List.of(
+                new User(1, "Mart", new Account(1001)),
+                new User(5, "Peter", new Account(1003)),
+                new User(9, "Jame", new Account(1004)),
+                new User(2, "Silva", new Account(1002)),
+                new User(8, "Poppy", new Account(1005))
+        );
+        for (var u : users) {
+            userIdMapAccount.put(u.id(), u.account());
+        }
+        System.out.println(userIdMapAccount);
+    }
+}
