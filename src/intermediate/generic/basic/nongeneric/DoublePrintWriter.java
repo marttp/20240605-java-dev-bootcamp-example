cef9f78090ec5cf9f4f9a342118c2cc29ab4f4ca
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-13 12:12:34 +0900

    Generics - Non Generic PrintWriter example

---
+++ src/intermediate/generic/basic/nongeneric/DoublePrintWriter.java
@@ -0,0 +1,8 @@
+package intermediate.generic.basic.nongeneric;
+
+public class DoublePrintWriter {
+
+    public void print(double data) {
+        System.out.println(data);
+    }
+}
---
+++ src/intermediate/generic/basic/nongeneric/IntPrintWriter.java
@@ -0,0 +1,8 @@
+package intermediate.generic.basic.nongeneric;
+
+public class IntPrintWriter {
+
+    public void print(int data) {
+        System.out.println(data);
+    }
+}
