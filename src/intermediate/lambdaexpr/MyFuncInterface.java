645e03c3f831890bd06aa82c6753299570545c18
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-06-13 21:44:51 +0900

    Lambda expression - MyFuncInterface

---
+++ src/intermediate/lambdaexpr/MyFuncInterface.java
@@ -0,0 +1,8 @@
+package intermediate.lambdaexpr;
+
+@FunctionalInterface
+public interface MyFuncInterface {
+
+    // Java Interface contains one and only one abstract method
+    double doubleValue(int n);
+}
